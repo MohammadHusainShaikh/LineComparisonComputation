b6eeeade501bbc72ed8a4919062ee9aa02aae8da
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-09 13:32:20 +0530

    [MODIFIED];Husain - Check equality of two lines

--- src/com/infoglaxy/linecomp/LineComparisonComputation.java
+++ src/com/infoglaxy/linecomp/LineComparisonComputation.java
@@ -3,28 +3,48 @@ package com.infoglaxy.linecomp;
 import java.util.Scanner;
 
 public class LineComparisonComputation {
+	
+	int x1;
+	int x2;
+	int y1;
+	int y2;
+	//double length;
 
-	public static void main(String[] args) {
+	 
+		public void input() {
+			System.out.println("Enter the X1,Y1 and X1,Y2 Points for the Line:");
+			Scanner scanner = new Scanner(System.in);
+			x1 = scanner.nextInt();
+			y1 = scanner.nextInt();
+			x2 = scanner.nextInt();
+			y2 = scanner.nextInt();
 		
-		System.out.println("Welcome to Line Comparison Computation problem");
-		int x1;
-		int x2;
-		int y1;
-		int y2;
-		double length;
+		}
+		double lineCalculate() {
+			return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+			//System.out.print("Length of Line="+length);
+		}
 	
-		Scanner scanner = new Scanner(System.in);
+		public void checkEquality(LineComparisonComputation lineComparisonCoputation2) {
+			if(this.lineCalculate() == lineComparisonCoputation2.lineCalculate())
+			{
+				System.out.println("Line are same");
+			}else {
+				System.out.println("Line are not same");
+			}
+		}
 	
-		System.out.println("Enter the X1,Y1 and X1,Y2 Points for the Line:");
-		x1 = scanner.nextInt();
-		y1 = scanner.nextInt();
-		x2 = scanner.nextInt();
-		y2 = scanner.nextInt();
 	
-		// Length of Line  = sqrt ( (x2 - x1) ^ 2 + (y2 - y1) ^ 2)
-		length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-		System.out.print("Length of Line="+length);
-
-	}
+		
+	
+		public static void main(String[] args) {
+			System.out.println("Welcome to Line Comparison Computation problem");
+			LineComparisonComputation lineComparisonComputation = new LineComparisonComputation();
+			lineComparisonComputation.input();
+			LineComparisonComputation lineComparisonComputation2 = new LineComparisonComputation();
+			lineComparisonComputation2.input();
+			
+			lineComparisonComputation.checkEquality(lineComparisonComputation2);
+		}
 
 }
