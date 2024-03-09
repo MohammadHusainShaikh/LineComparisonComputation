2194f38e004666e26e32965c430d5bdd93358f65
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-09 13:05:32 +0530

    [MODIFIED];Husain - Calculated length of line

--- src/com/infoglaxy/linecomp/LineComparisonComputation.java
+++ src/com/infoglaxy/linecomp/LineComparisonComputation.java
@@ -1,12 +1,29 @@
 package com.infoglaxy.linecomp;
 
+import java.util.Scanner;
 
 public class LineComparisonComputation {
 
 	public static void main(String[] args) {
 		
-	System.out.println("Welcome to Line Comparison Computation problem");
+		System.out.println("Welcome to Line Comparison Computation problem");
+		int x1;
+		int x2;
+		int y1;
+		int y2;
+		double length;
 	
+		Scanner scanner = new Scanner(System.in);
+	
+		System.out.println("Enter the X1,Y1 and X1,Y2 Points for the Line:");
+		x1 = scanner.nextInt();
+		y1 = scanner.nextInt();
+		x2 = scanner.nextInt();
+		y2 = scanner.nextInt();
+	
+		// Length of Line  = sqrt ( (x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+		length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		System.out.print("Length of Line="+length);
 
 	}
 
