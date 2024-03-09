0e07fbcdae4785704d0192517ee320e226a6db5c
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-09 12:51:35 +0530

    [ADDED]:Husain - Displaying welcome massage

---
+++ src/com/infoglaxy/linecomp/LineComparisonComputation.java
@@ -0,0 +1,13 @@
+package com.infoglaxy.linecomp;
+
+
+public class LineComparisonComputation {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Welcome to Line Comparison Computation problem");
+	
+
+	}
+
+}
